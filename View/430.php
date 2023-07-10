6daa24f5bb88a1093ed132cc1e7e3036aa374e2b
Louis Ouellet <louis_ouellet@hotmail.com>
2023-07-10 13:16:14 -0400

    2023-07-10 13:16 - v0.0.31

--- View/430.php
+++ View/430.php
@@ -2,11 +2,11 @@
 <h3 class="mb-3">Sign In</h3>
 <form method="post" class="w-100">
     <div class="form-floating my-3">
-        <input type="text" name="username" class="form-control form-control-lg" placeholder="username@domain.com" id="username">
+        <input type="text" name="username" class="form-control form-control-lg" style="width:350px;" placeholder="username@domain.com" id="username" autocomplete="current-username">
         <label for="username">Username</label>
     </div>
     <div class="form-floating my-3">
-        <input type="password" name="password" class="form-control form-control-lg" placeholder="*******************" id="password">
+        <input type="password" name="password" class="form-control form-control-lg" style="width:350px;" placeholder="*******************" id="password" autocomplete="current-password">
         <label for="password">Password</label>
     </div>
     <div class="form-check my-3 mb-5 form-switch">
@@ -17,7 +17,7 @@
     <button type="submit" name="login" class="btn btn-primary w-100">Sign In</button>
 </form>
 <div class="d-flex align-items-center justify-content-between w-100 mt-3">
-    <a href="/forgot">I forgot my password</a>
-    <a href="/register">Register</a>
+    <a href="/forgot"><i class="bi bi-question-diamond me-1"></i>I forgot my password</a>
+    <a href="/register"><i class="bi bi-person-plus me-1"></i>Register</a>
 </div>
 <!-- ======= End Sign In ======= -->
