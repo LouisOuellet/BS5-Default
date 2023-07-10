19370c7260f4722b97641e1bab2946b3b0d0bf23
Louis Ouellet <louis_ouellet@hotmail.com>
2023-07-10 13:24:21 -0400

    2023-07-10 13:24 - v0.0.32

--- View/430.php
+++ View/430.php
@@ -13,11 +13,11 @@
         <input class="form-check-input" style="margin-left: -1.4em; margin-right: 1.4em;transform: scale(1.8);" type="checkbox" role="switch" name="remember" id="remember">
         <label class="form-check-label" for="remember">Remember me</label>
     </div>
-    <input type="hidden" class="d-none" name="csrf" value="">
+    <input type="hidden" class="d-none" name="csrf" value="<?= $this->CSRF->token() ?>">
     <button type="submit" name="login" class="btn btn-primary w-100">Sign In</button>
 </form>
 <div class="d-flex align-items-center justify-content-between w-100 mt-3">
-    <a href="/forgot"><i class="bi bi-question-diamond me-1"></i>I forgot my password</a>
-    <a href="/register"><i class="bi bi-person-plus me-1"></i>Register</a>
+    <a href="/forgot"><i class="bi bi-chevron-left me-1"></i>I forgot my password</a>
+    <a href="/register">Register<i class="bi bi-chevron-right ms-1"></i></a>
 </div>
 <!-- ======= End Sign In ======= -->
