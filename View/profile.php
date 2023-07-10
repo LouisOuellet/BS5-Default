6a5b81c9eca83749a6178269334ce03ebb9e9a1b
Louis Ouellet <louis_ouellet@hotmail.com>
2023-07-10 14:45:55 -0400

    2023-07-10 14:45 - v0.0.36

--- View/profile.php
+++ View/profile.php
@@ -5,5 +5,26 @@
         <div class="col-8 col-lg-9" id="col2"></div>
     </div>
 </div>
+<script>
+    // Load the profile data
+    const Profile = {
+        "id": <?= $this->Auth->User->get('id') ?>,
+        "created": "<?= $this->Auth->User->get('created') ?>",
+        "modified": "<?= $this->Auth->User->get('modified') ?>",
+        "username": "<?= $this->Auth->User->get('username') ?>",
+        "name": "<?= $this->Auth->User->get('name') ?>",
+        "address": "<?= $this->Auth->User->get('address') ?>",
+        "city": "<?= $this->Auth->User->get('city') ?>",
+        "state": "<?= $this->Auth->User->get('state') ?>",
+        "country": "<?= $this->Auth->User->get('country') ?>",
+        "zipcode": "<?= $this->Auth->User->get('zipcode') ?>",
+        "phone": "<?= $this->Auth->User->get('phone') ?>",
+        "mobile": "<?= $this->Auth->User->get('mobile') ?>",
+        "status": <?= $this->Auth->User->get('status') ?>,
+        "database": "<?= $this->Auth->User->get('database') ?>",
+        "server": "<?= $this->Auth->User->get('server') ?>",
+        "domain": "<?= $this->Auth->User->get('domain') ?>"
+    };
+</script>
 <script src="/js/profile.js"></script>
 <!-- ======= End Profile ======= -->
--- example/profile.php
+++ example/profile.php
@@ -5,5 +5,26 @@
         <div class="col-8 col-lg-9" id="col2"></div>
     </div>
 </div>
+<script>
+    // Load the profile data
+    const Profile = {
+        "id": 1,
+        "created": "2023-04-10 11:40:05",
+        "modified": "2023-05-04 12:19:03",
+        "username": "louis@laswitchtech.com",
+        "name": "Louis Ouellet",
+        "address": "15 rue Richardson",
+        "city": "Beauharnois",
+        "state": "Quebec",
+        "country": "Canada",
+        "zipcode": "J6N 2S9",
+        "phone": "4502250426",
+        "mobile": "5145940414",
+        "status": 7,
+        "database": null,
+        "server": null,
+        "domain": "laswitchtech.com",
+    };
+</script>
 <script src="/js/<?= $page ?>.js"></script>
 <!-- ======= End Profile ======= -->
